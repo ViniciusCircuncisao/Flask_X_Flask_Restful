fa2fc562c7544693fb186adef38470836a16cc66
unknown <viniciusfelipe2015.vc@gmail.com>
2024-09-26 00:38:09 -0300

    API desenvolvida para verificar diferenças da utilização de Flask puro para Flask Restful com conceitos básicos.

---
+++ app_restful.py
@@ -0,0 +1,60 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+import json
+
+app = Flask(__name__)
+
+api = Api(app)
+
+desenvolvedores = [
+    {'id': '0',
+     'nome': 'Vinicius',
+     'habilidades': ['Python', 'Flask']
+     },
+    {'id': '1',
+     'nome': 'João',
+     'habilidades': ['Python', 'Django']}
+]
+
+class Desenvolvedor(Resource):
+    def get(self, id):
+        try:
+            response = desenvolvedores[id]
+        except IndexError as e:
+            mensagem = f'Desenvolvedor de id: {id} não existe'
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status': 'erro', 'mensagem': mensagem}
+
+        return response
+
+    def put(self, id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+
+        return dados
+
+    def delete(self, id):
+        desenvolvedores.pop(id)
+
+        return {'status': 'Sucess', 'mensagem': 'Registro excluído'}
+
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return desenvolvedores
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+
+        return desenvolvedores[posicao]
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>')
+
+api.add_resource(ListaDesenvolvedores, '/dev')
+
+if __name__ == '__main__':
+    app.run(debug=True)
